f523e807ee9e9039b64c585c8d58634e6f8d7959
rwgit2023 <ronangit2023@hotmail.com>
2023-10-27 16:49:32 -0300

    create card depoiment

--- src/app/shared/card-depoimento/card-depoimento.component.ts
+++ src/app/shared/card-depoimento/card-depoimento.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CardDepoimentoComponent {
 
+  depoimento: string = `
+  Recomendo fortemente a agência de viagens Jornada.
+  Eles oferecem um serviço personalizado e de alta qualidade
+  que excedeu minhas expectativas em minha última viagem.
+`
+  autoria : string = 'Mariana Faustino'
+  
 }
